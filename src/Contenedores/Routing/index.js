f446c2836e20c0099c6c3e34e805df268371e50b
Herbert Fernandez Tamayo <hftamayo@gmail.com>
2021-01-09 20:18:43 -0600

    updating routing/index.js

--- src/Contenedores/Routing/index.js
+++ src/Contenedores/Routing/index.js
@@ -3,7 +3,6 @@ import { HashRouter, Route, Switch } from "react-router-dom";
 import Workers from '../Workers';
 import Medcerts from '../Medcerts';
 import Vermedcerts from '../Vermedcerts';
-import Menu from './Menu';
 
 function Routing() {
     return (
